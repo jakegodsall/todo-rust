376e32637eac92c4abe43ef9db94a889db98e969
Jake Godsall <jake.edward.godsall@gmail.com>
2025-07-26 10:04:08 +0200

    aded get option method

--- src/cli/handler.rs
+++ src/cli/handler.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use crate::models::todo::ToDoItem;
 
 pub fn print_todos(todos: &mut Vec<ToDoItem>) {
@@ -21,5 +23,51 @@ pub fn checkbox(condition: bool) -> &'static str {
         "☑"
     } else {
         "☐"
+    }
+}
+
+pub fn show_options() {
+    println!("----- WELCOME -----");
+    println!("Select an option: ");
+    println!("1. View todo items");
+    println!("2. Add a todo item");
+    println!("3. Check off a todo item");
+    println!("Quit");
+}
+
+pub fn get_option() -> i32 {
+    let valid_options = vec![1, 2, 3];
+
+    loop {
+        let mut selected_option = String::new();
+        io::stdin().read_line(&mut selected_option).expect("Line could not be read");
+        let selected_option: i32 = selected_option.trim().parse().unwrap_or(-1);
+
+        if selected_option == -1 {
+            continue
+        }
+
+        if valid_options.contains(&selected_option) {
+            return selected_option
+        }
+    }
+}
+
+pub fn main_loop() {
+    show_options();
+
+    loop {
+        let mut selected_option = String::new();
+        io::stdin().read_line(&mut selected_option).expect("Line could not be read");
+        let selected_option = selected_option.trim().to_string();
+
+        let selected_option: i32 = selected_option.parse().unwrap();
+
+        let mut valid_options = vec![1, 2, 3];
+
+        if valid_options.contains(&selected_option) {
+
+        }
+        
     }
 }
