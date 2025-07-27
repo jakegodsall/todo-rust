63648f6b64b00ad43740b7f1cc423127897b0446
jakegodsall <jake.edward.godsall@gmail.com>
2025-07-27 08:51:59 +0200

    update to use OpenOptions

--- src/export/plaintext.rs
+++ src/export/plaintext.rs
@@ -1,13 +1,19 @@
 use crate::models::todo::{ ToDoItem };
-use std::fs::{ File };
+use std::fs::{ File, OpenOptions };
 use std::io::{ Write, BufWriter };
 
-pub fn export(filename: &str, todos: &Vec<ToDoItem>) {
-    let file = File::create(filename).expect("Could not create file");
+pub fn export(filename: &str, todos: &Vec<ToDoItem>) -> std::io::Result<()> {
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&filename)?;
+
     let mut writer = BufWriter::new(file);
 
     for todo in todos {
         writeln!(writer, "{}", todo.string_repr())
             .expect("Failed to write to file");
     }
+
+    Ok(())
 }
